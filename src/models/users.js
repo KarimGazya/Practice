a569cb3d82098847b49068a3b5217e024fb7f31f
Mikhail George Samir <mesogeorge2013@gmail.com>
2024-10-11 23:57:54 +0200

    starting

---
+++ src/models/users.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userschema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+
+  password: {
+    type: String,
+    required: true,
+  },
+
+  email: {
+    type: String,
+    //required: true,
+    unique: true,
+  },
+
+  role: {
+    type: String,
+    enum: [
+      "Tourist",
+      "Tour Guide",
+      "Seller",
+      "Advertiser",
+      "Tourism Governor",
+      "Admin",
+    ],
+    required: true,
+  },
+});
+
+const user = mongoose.model("users", userschema);
+module.exports = user;
